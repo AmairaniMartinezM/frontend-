d6614587a5426140ba339abf0fc63fb251aa8021
AmairaniMartinezM <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-11-14 12:55:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,25 @@ def index():
     message = "Hello, World"
     return render_template('index.html', message=message)
 
-@app.route("/buscar",methods=["GET","POST"])
+@app.route("/buscar.html",methods=["GET","POST"])
 def buscar():
     return render_template('buscar.html')
 
-@app.route("/prueba")
-def prueba():
-    return render_template('prueba.html')
+@app.route("/templates/borrar.html",methods=["GET", "DELETE"])
+def borrar():
+    return render_template('borrar.html')
+
+@app.route("/templates/editar.html",methods=["GET", "PUT"])
+def editar():
+    return render_template('editar.html')
+
+@app.route("/insertar.html",methods=["GET", "POST"])
+def insertar():
+    return render_template('insertar.html')
+
+@app.route("/templates/ver.html", methods=["GET"])
+def ver():
+    return render_template('ver.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
